acfd50ffd0e82e9b42d4e13e1716d430ce47dad0
agent 47，here is a mission for you <675767685@qq.com>
2016-07-04 18:10:30 +0800

    Create regression_tree.py

---
+++ regression_tree/regression_tree.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+# -*- coding:  utf-8 -*
+"""
+This module is used to learn CART algorithm
+Authors: shiduo(shiduo@baidu.com)
+Date:2016-6-12
+"""
+from numpy import *
+import matplotlib.pyplot as plt
+
+def loadDataSet(fileName):      
+    dataMat = []               
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float,curLine) #map all elements to float()
+        dataMat.append(fltLine)
+    return dataMat
+
+def binSplitDataSet(dataSet, feature, value):
+    dataMat=mat(dataSet)
+#     if len(nonzero(dataSet[:,feature] > value)[0])>0:
+    lie=dataMat[:,feature]
+    mat0 = dataMat[nonzero(lie > value)[0],:]
+#     if len(nonzero(dataSet[:,feature] <= value)[0])>0:
+    mat1 = dataMat[nonzero(lie <= value)[0],:]
+    return mat0,mat1
+def regLeaf(dataSet):#returns the value used for each leaf
+    """
+    function:
+        sample average
+    """
+    return mean(dataSet[:,-1])
+
+def regErr(dataSet):
+    """
+       sample variance 
+    """
+    return var(dataSet[:,-1]) * shape(dataSet)[0]
+
+def linearSolve(dataSet):   
+    
+    m,n = shape(dataSet)
+    X = mat(ones((m,n))); Y = mat(ones((m,1))) #create a copy of data with 1 in 0th postion
+    X[:,1:n] = dataSet[:,0:n-1]; Y = dataSet[:,-1]#and strip out Y
+    xTx = X.T*X
+    if linalg.det(xTx) == 0.0:
+        raise NameError('This matrix is singular, cannot do inverse,\n\
+        try increasing the second value of ops')
+    ws = xTx.I * (X.T * Y)
+    return ws,X,Y
+    
+def modelLeaf(dataSet):#create linear model and return coeficients
+    ws,X,Y = linearSolve(dataSet)
+    return ws
+
+def modelErr(dataSet):
+    ws,X,Y = linearSolve(dataSet)
+    yHat = X * ws
+    return sum(power(Y - yHat,2))
+
+def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
+    tolS = ops[0]; tolN = ops[1]
+    dataMat=mat(dataSet)
+    #if all the target variables are the same value: quit and return value
+    if len(set(dataMat[:,-1].T.tolist()[0])) == 1: #exit cond 1
+        return None, leafType(dataMat)
+    m,n = shape(dataMat)
+    #the choice of the best feature is driven by Reduction in RSS error from mean
+    S = errType(dataMat)
+    bestS = inf; bestIndex = 0; bestValue = 0
+    for featIndex in range(n-1):
+        x=dataMat[:,featIndex]
+        for splitVal in x:
+            mat0, mat1 = binSplitDataSet(dataMat, featIndex, splitVal)
+            if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN): 
+                continue
+            newS = errType(mat0) + errType(mat1)
+            if newS < bestS: 
+                bestIndex = featIndex
+                bestValue = round(splitVal[0,0],2)
+                bestS = newS
+    #if the decrease (S-bestS) is less than a threshold don't do the split
+    if (S - bestS) < tolS: 
+        return None, leafType(dataMat) #exit cond 2
+    mat0, mat1 = binSplitDataSet(dataMat, bestIndex, bestValue)
+    if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):  #exit cond 3
+        return None, leafType(dataMat)
+    return bestIndex,bestValue#returns the best feature to split on
+                              #and the value used for that split
+
+def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
+    feat, val = chooseBestSplit(dataSet, leafType, errType, ops)#choose the best split
+    if feat == None: return val #if the splitting hit a stop condition return val
+    retTree = {}
+    retTree['spInd'] = feat
+    retTree['spVal'] = val
+    dataMat=mat(dataSet)
+    lSet, rSet = binSplitDataSet(dataMat, feat, val)
+    retTree['left'] = createTree(lSet, leafType, errType, ops)
+    retTree['right'] = createTree(rSet, leafType, errType, ops)
+    return retTree  
+def isTree(obj):
+    return (type(obj).__name__=='dict')
+
+def getMean(tree):
+    if isTree(tree['right']): tree['right'] = getMean(tree['right'])
+    if isTree(tree['left']): tree['left'] = getMean(tree['left'])
+    return (tree['left']+tree['right'])/2.0
+    
+def prune(tree, testData):
+    if shape(testData)[0] == 0: 
+        return getMean(tree) #if we have no test data collapse the tree
+    if (isTree(tree['right']) or isTree(tree['left'])):#if the branches are not trees try to prune them
+        lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+    if isTree(tree['left']): 
+        tree['left'] = prune(tree['left'], lSet)
+    if isTree(tree['right']): 
+        tree['right'] =  prune(tree['right'], rSet)
+    #if they are now both leafs, see if we can merge them
+    if not isTree(tree['left']) and not isTree(tree['right']):
+        lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+        errorNoMerge = sum(power(lSet[:,-1] - tree['left'],2)) +\
+            sum(power(rSet[:,-1] - tree['right'],2))
+        treeMean = (tree['left']+tree['right'])/2.0
+        errorMerge = sum(power(testData[:,-1] - treeMean,2))
+        if errorMerge < errorNoMerge: 
+            print "merging"
+            return treeMean
+        else: return tree
+    else: return tree
+def regTreeEval(model, inDat):
+    return float(model)
+
+def modelTreeEval(model, inDat):
+    n = shape(inDat)[1]
+    X = mat(ones((1,n+1)))
+    X[:,1:n+1]=inDat
+    return float(X*model)
+
+def treeForeCast(tree, inData, modelEval=regTreeEval):
+    if not isTree(tree): return modelEval(tree, inData)
+    if inData[0,0] > tree['spVal']:
+        if isTree(tree['left']): return treeForeCast(tree['left'], inData, modelEval)
+        else: return modelEval(tree['left'], inData)
+    else:
+        if isTree(tree['right']): return treeForeCast(tree['right'], inData, modelEval)
+        else: return modelEval(tree['right'], inData)
+        
+def createForeCast(tree, testData, modelEval=regTreeEval):
+    testData=mat(testData)
+    m,n=testData.shape
+    xMat=mat(dataSet)[:,0]
+    xCopy=xMat.copy()
+    xCopy.sort(0)
+    yHat = mat(zeros((m,1)))
+    for i in range(m):
+        yHat[i,0] = treeForeCast(tree, xCopy[i], modelEval)
+    return yHat,xCopy
+def make_plot(xMat,yMat,xCopy,yHat):
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    ax.scatter(xMat,yMat)
+    ax.plot(xCopy,yHat,color="red")
+    plt.show()
+if __name__ == '__main__':
+    dataSet=loadDataSet('../data/ex2test.txt')
+    retTree = createTree(dataSet)
+    print retTree
+    retTree = prune(retTree,dataSet)
+    yHat,xCopy= createForeCast(retTree,dataSet)
+    xMat=mat(dataSet)[:,0]
+    yMat=mat(dataSet)[:,1]
+    make_plot(xMat, yMat, xCopy, yHat)
+
+    retTree = createTree(dataSet,leafType=modelLeaf, errType=modelErr, ops=(1,4))
+    print retTree
+    yHat,xCopy= createForeCast(retTree,dataSet,modelEval=modelTreeEval)
+    xMat=mat(dataSet)[:,0]
+    yMat=mat(dataSet)[:,1]
+    make_plot(xMat, yMat, xCopy, yHat)
